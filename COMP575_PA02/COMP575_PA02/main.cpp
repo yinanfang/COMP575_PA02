d73d9b11324ee999efc19e843b03eedad6f2c464
yinanfang <yinan_fang@hotmail.com>
2014-10-01 08:06:55 -0400

    init...

---
+++ COMP575_PA02/COMP575_PA02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP575_PA02
+//
+//  Created by Golden Compass on 10/1/14.
+//  Copyright (c) 2014 Golden Compass. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
